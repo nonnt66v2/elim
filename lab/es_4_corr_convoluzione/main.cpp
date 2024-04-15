d11fdbcb2a4043f6710c60135efa1c22414eebb0
nonnt66v2 <91503962+nonnt66v2@users.noreply.github.com>
2024-04-15 18:15:32 +0200

    es_4 finito

---
+++ lab/es_4_corr_convoluzione/main.cpp
@@ -0,0 +1,57 @@
+//
+// Created by nonnt66 on 15/04/24.
+//
+#include <stdlib.h>
+#include <opencv4/opencv2/opencv.hpp>
+
+using namespace cv;
+
+void correlazione(cv::Mat *k,cv::Mat *m,int k_rows, int k_cols, int x, int y){
+    float somma=0;
+    for (int s = 0; s < k_rows; s++) {
+        for (int t = 0; t < k_cols; t++) {
+            somma += ((k->at<float>(s, t) * m->at<float>(x + s, y + t)));
+        }
+    }
+    m->at<float>(x, y) = somma;
+}
+int main() {
+    cv::Mat m = cv::Mat::zeros(10, 10, CV_32F);
+    //cv::randu(m, 0, 1);
+    m.at<float>(5, 5) = 1;
+//    std::cout << m << std::endl;
+    copyMakeBorder(m, m, 3, 3, 3, 3, 0);
+//    std::cout << m << std::endl;
+    ///creo la maschera
+    cv::Mat k(3, 3, CV_32F);
+    int tmp = 1;
+    for (int i = 0; i < k.cols; ++i) {
+        for (int j = 0; j < k.rows; ++j) {
+            k.at<float>(i, j) = (float) tmp++;
+        }
+    }
+    std::cout << m.at<float>(6, 6) << std::endl;
+    std::cout << k.rows << std::endl;
+
+
+    float somma = 0;
+
+    ///accedo a tutti gli intorni dei pixel della maschera
+    for (int i = 1; i < m.rows - 1; i++) {
+        for (int j = 1; j < m.cols - 1; j++) {
+            correlazione(&k,&m,k.rows,k.cols,i,j);
+        }
+    }
+    std::cout << m << std::endl;
+    m = cv::Mat::zeros(10,10,CV_32F);
+    m.at<float>(5, 5) = 1;
+//   std::cout << m << std::endl;
+    copyMakeBorder(m, m, 1, 1, 1, 1, 0);
+    filter2D(m,m,CV_32F,k,cv::Point(-1,-1),0,BORDER_DEFAULT);
+
+    std::cout << m << std::endl;
+
+
+
+}
+
