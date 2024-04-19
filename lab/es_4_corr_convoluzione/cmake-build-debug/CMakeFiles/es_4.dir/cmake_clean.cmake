28aa140c2fc8f7434749c262b2d1cc62cb211a70
nonnt66 <lorenzo.dipalo001@studenti.uniparthenope.it>
2024-04-19 17:07:29 +0200

    filtraggio_spaziale2

---
+++ lab/es_4_corr_convoluzione/cmake-build-debug/CMakeFiles/es_4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/es_4.dir/main.cpp.o"
+  "CMakeFiles/es_4.dir/main.cpp.o.d"
+  "es_4"
+  "es_4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/es_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- lab/es_4_corr_convoluzione/main.cpp
+++ lab/es_4_corr_convoluzione/main.cpp
@@ -6,8 +6,8 @@
 
 using namespace cv;
 
-void correlazione(cv::Mat *k,cv::Mat *m,int k_rows, int k_cols, int x, int y){
-    float somma=0;
+void correlazione(cv::Mat *k, cv::Mat *m, int k_rows, int k_cols, int x, int y) {
+    float somma = 0;
     for (int s = 0; s < k_rows; s++) {
         for (int t = 0; t < k_cols; t++) {
             somma += ((k->at<float>(s, t) * m->at<float>(x + s, y + t)));
@@ -15,11 +15,12 @@ void correlazione(cv::Mat *k,cv::Mat *m,int k_rows, int k_cols, int x, int y){
     }
     m->at<float>(x, y) = somma;
 }
+
 int main() {
     cv::Mat m = cv::Mat::zeros(10, 10, CV_32F);
     //cv::randu(m, 0, 1);
     m.at<float>(5, 5) = 1;
-//    std::cout << m << std::endl;
+    std::cout << m << std::endl;
     copyMakeBorder(m, m, 3, 3, 3, 3, 0);
 //    std::cout << m << std::endl;
     ///creo la maschera
@@ -31,26 +32,22 @@ int main() {
         }
     }
     std::cout << m.at<float>(6, 6) << std::endl;
-    std::cout << k.rows << std::endl;
 
-
-    float somma = 0;
 
     ///accedo a tutti gli intorni dei pixel della maschera
     for (int i = 1; i < m.rows - 1; i++) {
         for (int j = 1; j < m.cols - 1; j++) {
-            correlazione(&k,&m,k.rows,k.cols,i,j);
+            correlazione(&k, &m, k.rows, k.cols, i, j);
         }
     }
     std::cout << m << std::endl;
-    m = cv::Mat::zeros(10,10,CV_32F);
+    m = cv::Mat::zeros(10, 10, CV_32F);
     m.at<float>(5, 5) = 1;
 //   std::cout << m << std::endl;
     copyMakeBorder(m, m, 1, 1, 1, 1, 0);
-    filter2D(m,m,CV_32F,k,cv::Point(-1,-1),0,BORDER_DEFAULT);
+    filter2D(m, m, CV_32F, k, cv::Point(-1, -1), 0, BORDER_DEFAULT);
 
     std::cout << m << std::endl;
-
 
 
 }
